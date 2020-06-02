9f89135b283efa561809bcc8a6708ebf3534ee07
wsk123 <3229472262@qq.com>
2020-06-02 17:02:03 +0800

    chushihau

---
+++ src/main/java/com/wsk/SsasApplication.java
@@ -0,0 +1,13 @@
+package com.wsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsasApplication.class, args);
+    }
+
+}
